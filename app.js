f31dd46f594077b290344cd0bc469b578f6d03a0
Abdujalol Nabijonov <mrarnoldunlimited@gmail.com>
2023-09-16 04:45:03 +0900

    feat:

---
+++ app.js
@@ -0,0 +1,20 @@
+const animals=['fox','ant','bird','lion','deer','wolf','bear',
+'frog','hen','mole','duck','goat','dog','cat','bat','cock','cow'];
+
+
+function howManyAnimalsHave(text){
+    return animals.filter((a)=>canWeCreateWord(a,text))
+}
+
+function canWeCreateWord(animal,text){
+    let index=0
+    while(index<animal.length){
+        if(!text.includes(animal[index])){
+            return false
+        }
+        index++
+    }
+    return true
+}
+const javob =howManyAnimalsHave('fdgwocalt');
+console.log('javob:',javob)
