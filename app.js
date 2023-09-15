e38b8518eab12e004f18ade7ada5db3438eda6f6
Abdujalol Nabijonov <mrarnoldunlimited@gmail.com>
2023-09-16 04:51:49 +0900

    fix:

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const animals=['fox','ant','bird','lion','deer','wolf','bear',
+const animals = ['fox','ant','bird','lion','deer','wolf','bear',
 'frog','hen','mole','duck','goat','dog','cat','bat','cock','cow'];
 
 
 function howManyAnimalsHave(text){
-    return animals.filter((a)=>canWeCreateWord(a,text))
+    return animals.filter((a) => canWeCreateWord(a,text))
 }
 
 function canWeCreateWord(animal,text){
-    let index=0
+    let index = 0;
     while(index<animal.length){
         if(!text.includes(animal[index])){
             return false
         }
-        index++
+        index++;
     }
     return true
 }
-const javob =howManyAnimalsHave('fdgwocalt');
-console.log('javob:',javob)
+const javob = howManyAnimalsHave('fdgwocalt');
+console.log('javob:',javob);
